57a6dcd1c4bbec5e042111fdd0262058b67c1c8e
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-28 16:30:32 +0600

    added call system

--- script.js
+++ script.js
@@ -12,35 +12,71 @@ const copyBtn = document.getElementsByClassName("copy-btn");
 const heartBtn = document.getElementsByClassName("heart");
 const clearBtn = document.getElementById("clear-btn");
 
+// call
+
+for (let btn of callBtn) {
+  btn.addEventListener("click", function () {
+      if (coin < 0) {
+          alert("❌ you don't have enough coin. you need 20 coin to call");
+          return;
+        }
+        
+    coin -= 20;
+    coinCount.innerText = coin;
+
+    const card = btn.closest("div#card");
+
+    const service = card.querySelector(".service").innerText;
+    const number = card.querySelector(".number").innerText;
+
+    alert(`📞 Calling ${service} ${number}...`);
+
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+
+    const li = document.createElement("div");
+    li.innerHTML = `
+        <div class="bg-gray-100 rounded-xl flex justify-between items-center p-4 space-y-3 ">
+            <div>
+                <h1 class = "font-bold">${service}</h1>
+                <p>${number}</p>
+            </div>
+            <div>
+                ${timeString}
+            </div>
+          </div>
+    `;
+    historyList.appendChild(li);
+  });
 }
 
 // copy
 
-for(let btnC of copyBtn){
-    btnC.addEventListener("click", function(){
+for (let btnC of copyBtn) {
+  btnC.addEventListener("click", function () {
     copy += 1;
     copyCount.innerText = copy;
-})
+  });
 }
 
 // heart
 
-for(let btnH of heartBtn){
-    btnH.addEventListener("click", function(){
+for (let btnH of heartBtn) {
+  btnH.addEventListener("click", function () {
     heart += 1;
     heartCount.innerText = heart;
-})
+  });
 }
 
 // clear
 
-clearBtn.addEventListener("click", function(){
-    heart = 0;
-    coin = 100;
-    copy = 0;
-    
-    heartCount.innerText = heart;
-    coinCount.innerText = coin;
-    copyCount.innerText = copy;
-    historyList.innerHTML = "";
-})
+clearBtn.addEventListener("click", function () {
+  heart = 0;
+  coin = 100;
+  copy = 0;
+
+  heartCount.innerText = heart;
+  coinCount.innerText = coin;
+  copyCount.innerText = copy;
+  historyList.innerHTML = "";
+});
